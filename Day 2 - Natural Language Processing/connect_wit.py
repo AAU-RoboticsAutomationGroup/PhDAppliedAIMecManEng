c3d9190b439c1ba027a03da6ad600844163e5816
lcroy <cl@m-tech.aau.dk>
2022-12-05 18:39:31 +0100

    update wit

--- Day 2 - Natural Language Processing/connect_wit.py
+++ Day 2 - Natural Language Processing/connect_wit.py
@@ -2,6 +2,16 @@ from wit import Wit
 
 client = Wit("KBYOLXIIU23MUV3ADAML27HRLYMQZK63")
 
-text = client.message('I need you to delivery this book to the lab')
+response = client.message('I need you deliver this computer to the lab')
 
-print(text['intents'][0]['name'])
+print(response)
+
+print("Intent is: " + response['intents'][0]['name'])
+
+if 'destination:destination' in response['entities']:
+
+    print("Destination is: " + response['entities']['destination:destination'][0]['value'])
+
+if 'object:object' in response['entities']:
+
+    print("Object is: " + response['entities']['object:object'][0]['value'])
